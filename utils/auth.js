78d281b39c7918fb40467c1c325ebffe3e47fdcc
laurachebet1997 <rosechebet19971997@gmail.com>
2023-04-02 19:59:14 +0300

    eslint

--- utils/auth.js
+++ utils/auth.js
@@ -1,3 +1,6 @@
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable no-unused-vars */
+
 import sha1 from 'sha1';
 import { Request } from 'express';
 import mongoDBCore from 'mongodb/lib/core';
